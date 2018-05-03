199da48fb079003e885d6651fe7a9e90e7ef1f1a
Fabian Meyer <fabian.meyer@studiointeractive.de>
2018-05-03 12:37:09 +0200

    Add support for onSlideShow/Hide events
    This commits adds event based handling of slide state. Each slide supports an onSlideShow and an onSlideHide callback.

--- src/slide.vue
+++ src/slide.vue
@@ -1,41 +1,86 @@
 <template>
-  <div :class="slideClass">
-    <slot></slot>
-  </div>
+    <div :class="slideClass">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'swiper-slide',
-    data: function() {
-      return {
-        slideClass: 'swiper-slide'
-      }
-    },
-    ready: function() {
-      this.update()
-    },
-    mounted: function() {
-      this.update()
-      if (this.$parent.options.slideClass) {
-        this.slideClass = this.$parent.options.slideClass
-      }
-    },
-    updated: function() {
-      this.update()
-    },
-    attached: function() {
-      this.update()
-    },
-    methods: {
-      update: function() {
-        if (this.$parent && this.$parent.swiper && this.$parent.swiper.update) {
-          this.$parent.swiper.update(true)
-          if (this.$parent.options.loop) {
-            this.$parent.swiper.reLoop()
-          }
+    export default {
+        name: 'swiper-slide',
+        props: {
+            onSlideShow: {
+                type: Function,
+                default: function () {
+                }
+            },
+            onSlideHide: {
+                type: Function,
+                default: function () {
+                }
+            },
+        },
+        data: function () {
+            return {
+                slideClass: 'swiper-slide',
+                slideIndexInParent: -1,
+                isActive: false
+            }
+        },
+        ready: function () {
+            this.update()
+        },
+        mounted: function () {
+            // determine slide index in parent (swiper)
+            this.$parent.$children.forEach((child, index) => {
+                if (child === this) {
+                    // console.log(index);
+                    this.slideIndexInParent = index;
+                }
+            });
+            //console.log('Hello, I am slide ' + this.slideIndexInParent);
+            this.update()
+            if (this.$parent.options.slideClass) {
+                this.slideClass = this.$parent.options.slideClass
+            }
+
+            this.$parent.$on('changeActiveSlide', this.onParentChangeActiveSlide);
+        },
+        destroyed: function () {
+            this.$parent.$off('changeActiveSlide', this.onParentChangeActiveSlide);
+        },
+        updated: function () {
+            this.update()
+        },
+        attached: function () {
+            this.update()
+        },
+        methods: {
+            update: function () {
+                if (this.$parent && this.$parent.swiper && this.$parent.swiper.update) {
+                    this.$parent.swiper.update(true)
+                    if (this.$parent.options.loop) {
+                        this.$parent.swiper.reLoop()
+                    }
+                }
+            },
+            onParentChangeActiveSlide: function (activeSlide) {
+                if (activeSlide == this.slideIndexInParent) {
+                    //console.log('I became active. I am ' + this.slideIndexInParent);
+                    this.isActive = true;
+                    if (typeof this.onSlideShow === 'function') {
+                        this.onSlideShow();
+                    }
+                } else {
+                    if (this.isActive) {
+                        if (typeof this.onSlideHide === 'function') {
+                            this.onSlideHide();
+                        }
+                        this.isActive = false;
+                    }
+
+
+                }
+            }
         }
-      }
     }
-  }
 </script>
--- src/swiper.vue
+++ src/swiper.vue
@@ -1,81 +1,104 @@
 <template>
-  <div class="swiper-container">
-    <slot name="parallax-bg"></slot>
-    <div :class="defaultSwiperClasses.wrapperClass">
-      <slot></slot>
+    <div class="swiper-container">
+        <slot name="parallax-bg"></slot>
+        <div :class="defaultSwiperClasses.wrapperClass">
+            <slot></slot>
+        </div>
+        <slot name="pagination"></slot>
+        <slot name="button-prev"></slot>
+        <slot name="button-next"></slot>
+        <slot name="scrollbar"></slot>
     </div>
-    <slot name="pagination"></slot>
-    <slot name="button-prev"></slot>
-    <slot name="button-next"></slot>
-    <slot name="scrollbar"></slot>
-  </div>
 </template>
 
 <script>
-  var browser = typeof window !== 'undefined'
-  if (browser) window.Swiper = require('swiper')
-  export default {
-    name: 'swiper',
-    props: {
-      options: {
-        type: Object,
-        default: function() {
-          return {
-            autoplay: 3500
-          }
-        }
-      },
-      notNextTick: {
-        type: Boolean,
-        default: function() {
-          return false
-        }
-      }
-    },
-    data: function() {
-      return {
-        defaultSwiperClasses: {
-          wrapperClass: 'swiper-wrapper'
-        }
-      }
-    },
-    ready: function() {
-      if (!this.swiper && browser) {
-        this.swiper = new Swiper(this.$el, this.options)
-      }
-    },
-    mounted: function() {
-      var self = this
-      var mount = function() {
-        if (!self.swiper && browser) {
-          delete self.options.notNextTick
-          var setClassName = false
-          for(var className in self.defaultSwiperClasses){
-            if (self.defaultSwiperClasses.hasOwnProperty(className)) {
-              if (self.options[className]) {
-                setClassName = true
-                self.defaultSwiperClasses[className] = self.options[className]
-              }
+    var browser = typeof window !== 'undefined'
+    if (browser) window.Swiper = require('swiper')
+    export default {
+        name: 'swiper',
+        props: {
+            options: {
+                type: Object,
+                default: function () {
+                    return {
+                        autoplay: 3500
+                    }
+                }
+            },
+            notNextTick: {
+                type: Boolean,
+                default: function () {
+                    return false
+                }
             }
-          }
-          var mountInstance = function() {
-            self.swiper = new Swiper(self.$el, self.options)
-          }
-          setClassName ? self.$nextTick(mountInstance) : mountInstance()
+        },
+        data: function () {
+            return {
+                defaultSwiperClasses: {
+                    wrapperClass: 'swiper-wrapper'
+                },
+                extendedOptions: {}
+            }
+        },
+        ready: function () {
+            this.extendOptions = Object.assign({}, this.options);
+            this.extendOptions.onSlideChangeEnd = this.onSlideChangeEnd;
+
+            if (!this.swiper && browser) {
+                this.swiper = new Swiper(this.$el, this.extendOptions)
+            }
+        },
+        mounted: function () {
+            var self = this;
+            this.extendOptions = Object.assign({}, this.options);
+            this.extendOptions.onSlideChangeEnd = this.onSlideChangeEnd;
+
+            var mount = function () {
+                if (!self.swiper && browser) {
+                    delete self.extendOptions.notNextTick
+                    var setClassName = false
+                    for (var className in self.defaultSwiperClasses) {
+                        if (self.defaultSwiperClasses.hasOwnProperty(className)) {
+                            if (self.extendOptions[className]) {
+                                setClassName = true
+                                self.defaultSwiperClasses[className] = self.extendOptions[className]
+                            }
+                        }
+                    }
+                    var mountInstance = function () {
+                        self.swiper = new Swiper(self.$el, self.extendOptions);
+                        self.$emit('changeActiveSlide', self.swiper.realIndex);
+                    }
+                    setClassName ? self.$nextTick(mountInstance) : mountInstance()
+                }
+            }
+            (this.extendOptions.notNextTick || this.notNextTick) ? mount() : this.$nextTick(mount)
+        },
+        updated: function () {
+
+            if (this.swiper) {
+                this.swiper.update()
+            }
+        },
+        beforeDestroy: function () {
+            if (this.swiper) {
+                this.swiper.destroy()
+                delete this.swiper
+            }
+        },
+        methods: {
+            onSlideChangeEnd: function (args) {
+                this.$emit('changeActiveSlide', this.swiper.realIndex);
+                // call callback from props
+                if (this.options.hasOwnProperty('onSlideChangeEnd') && typeof this.options.onSlideChangeEnd === 'function') {
+                    if (args) {
+                        this.options.onSlideChangeEnd(...args);
+                    } else {
+                        this.options.onSlideChangeEnd();
+                    }
+
+                }
+            }
         }
-      }
-      (this.options.notNextTick || this.notNextTick) ? mount() : this.$nextTick(mount)
-    },
-    updated: function() {
-      if (this.swiper) {
-        this.swiper.update()
-      }
-    },
-    beforeDestroy: function() {
-      if (this.swiper) {
-        this.swiper.destroy()
-        delete this.swiper
-      }
     }
-  }
 </script>
